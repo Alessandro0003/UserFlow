3cc7e744842ccf9092bf28154c07aef2d370a860
Alessandro0003 <alessandroquinta3@gmail.com>
2024-04-09 23:28:54 -0300

    🔥 feat: add react-router-dom for gerencies routes pages

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,10 +1,24 @@
 import { Box } from '@mui/material'
-import { SignInSide } from './components/SignInSide'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { HomeLogin } from './pages/HomeLogin'
+import { Account } from './pages/Account'
+import { Register } from './pages/Register'
+
+const queryClient = new QueryClient()
 
 export function App() {
   return (
-    <Box display="flex" justifyContent="center">
-      <SignInSide />
+    <Box>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomeLogin />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/account" element={<Account />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </Box>
   )
 }
